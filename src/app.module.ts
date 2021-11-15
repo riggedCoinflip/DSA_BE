ed3f43eb33057eb3ca57f17b176ddb0cea914ec5
riggedCoinflip <tikalker@gmail.com>
2021-11-15 17:14:59 +0100

    add dotenv support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharaktereModule } from './charaktere/charaktere.module';
 
 @Module({
-  imports: [CharaktereModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot(), CharaktereModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
