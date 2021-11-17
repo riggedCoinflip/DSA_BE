07d0df24d0c1d8db52063470ccfe6c3f863cd410
riggedCoinflip <tikalker@gmail.com>
2021-11-17 11:07:16 +0100

    move app.*.ts to app/app.*.ts, change imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AttributeModule } from 'src/modules/attribute/attribute.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CharaktereModule } from './hero/hero.module';
 
 @Module({
-    imports: [ConfigModule.forRoot(), CharaktereModule],
+    imports: [ConfigModule.forRoot(), AttributeModule],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Das ist das Backend von "das schlaue Auge"';
+    return 'Hello World';
   }
 }
