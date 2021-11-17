f713077cb5b3cef2d83eadab566ea9e2ce6cf37b
riggedCoinflip <tikalker@gmail.com>
2021-11-17 11:05:12 +0100

    fix typo

--- src/modules/attribute/dto/update-attribute.dto.ts
+++ src/modules/attribute/dto/update-attribute.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAttributeDto } from './create-attribute.dto';
 
-export class UpdateCatDto extends PartialType(CreateAttributeDto) {}
+export class UpdateAttributeDto extends PartialType(CreateAttributeDto) {}
