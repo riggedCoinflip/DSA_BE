661a9f85677e6468f6d39d540a748a3b8e131434
riggedCoinflip <tikalker@gmail.com>
2021-11-29 21:10:00 +0100

    remove unused api tag

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ async function bootstrap() {
     .setTitle('API')
     .setDescription('API description')
     .setVersion('1.0')
-    .addTag('api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
